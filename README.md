bea517af9036d5d09b4283c827302cc6dd9ba5af
rdeboer-pzOtJY <robindbor@gmail.com>
2024-03-23 17:36:52 -0700

    user command now can accept a range, and is thus usable in visual mode much more easily.
    user command now can accept a range, and is thus usable in visual mode much more easily.
    
    user command now can accept a range, and is thus usable in visual mode much more easily.

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # Git Remote URL:
-This _very_ simple plugin adds the user commands: CopyGithubLink and CopyGithubLinkWithBranchName to fetch the remote URL for main and your current branch respectively.
+This simple plugin adds the user commands: CopyGithubLink and CopyGithubLinkWithBranchName to fetch the remote URL for main and your current branch respectively.
 
 It also exposes 2 functions for use however you like.
 
 ```lua
 -- defined functions:
-require('git-remote-url').git_url_to_clipboard(optional_branch, optional_path)
-require('git-remote-url').get_git_remote_url(optional_branch, optional_path)
+require('git-remote-url').git_url_to_clipboard(optional_branch, optional_path, optional_line_start, optional_line_end)
+require('git-remote-url').get_git_remote_url(optional_branch, optional_path, optional_line_start, optional_line_end)
 ```
 
-The function can optionally be given a different branch or path as an argument,
+The functions can optionally be given a different branch or path as an argument,
 but it will default to the current branch and the current path of the current buffer.
 
 If supplied a branch as argument, it will include the line selection only if it matches the current branch.
@@ -24,20 +24,12 @@ return {
   opts = {},
   keys = {
     {
-      '<leader>yg',
-      function()
-        require('git-remote-url').git_url_to_clipboard("main")
-      end,
-      mode = '',
-      desc = 'Git Remote Url main branch'
+      '<leader>yg', ':CopyGithubLink<CR>'
+      mode = '', desc = 'Git Remote Url main branch',
     },
     {
-      '<leader>yG',
-      function()
-        require('git-remote-url').git_url_to_clipboard()
-      end,
-      mode = '',
-      desc = 'Git Remote Url'
+      '<leader>yG', ':CopyGithubLinkWithBranchName<CR>'
+      mode = '', desc = 'Git Remote Url',
     },
   },
 }
@@ -45,4 +37,3 @@ return {
 
 ### Notes:
 This is a very simple plugin that will be receiving updates, just wanted to see real quick how easy it was to create a plugin!
-```
--- lua/git-remote-url/init.lua
+++ lua/git-remote-url/init.lua
@@ -1,5 +1,6 @@
 local M = {}
-local function get_git_url_info(branch, local_path)
+
+local function get_git_url_info(branch, local_path, select_start, select_end)
 	local isCurrentPath = false
 	local isCurrentBranch = false
 	local path = local_path
@@ -14,7 +15,6 @@ local function get_git_url_info(branch, local_path)
 	if string.find(path, "oil://", 1, true) then
 		path = path:sub(7, -1)
 	end
-	-- ensure absolute path
 	path = vim.fn.fnamemodify(path:gsub("\n", ""), ':p')
 	local isDir = (vim.fn.isdirectory(path) == 1)
 	local forgit = path
@@ -41,8 +41,8 @@ local function get_git_url_info(branch, local_path)
 
 	local lnSuffix = ""
 	if not isDir and isCurrentPath and isCurrentBranch then
-		local stsel = vim.fn.line(".")
-		local endsel = vim.fn.line("v")
+		local stsel = select_start or vim.fn.line(".")
+		local endsel = select_end or vim.fn.line("v")
 		if stsel == endsel then
 			lnSuffix = "#L" .. stsel
 		elseif stsel < endsel then
@@ -71,8 +71,8 @@ local function build_git_url(git_url, branch, relgitpath, lnSuffix)
 	end
 end
 
-function M.get_git_remote_url(desired_branch, local_path)
-	local url_info = get_git_url_info(desired_branch, local_path) or {}
+function M.get_git_remote_url(desired_branch, local_path, select_start, select_end)
+	local url_info = get_git_url_info(desired_branch, local_path, select_start, select_end) or {}
 	local git_url = url_info.git_url or ""
 	local branch = url_info.branch or ""
 	local relgitpath = url_info.relgitpath or ""
@@ -80,18 +80,18 @@ function M.get_git_remote_url(desired_branch, local_path)
 	return build_git_url(git_url, branch, relgitpath, lnSuffix)
 end
 
-function M.git_url_to_clipboard(desired_branch, local_path)
-	vim.fn.setreg("+", M.get_git_remote_url(desired_branch, local_path))
+function M.git_url_to_clipboard(desired_branch, local_path, select_start, select_end)
+	vim.fn.setreg("+", M.get_git_remote_url(desired_branch, local_path, select_start, select_end))
 end
 
 M.setup = function(opts)
-	vim.api.nvim_create_user_command('CopyGithubLink', function()
-		M.git_url_to_clipboard("main")
-	end, {})
+	vim.api.nvim_create_user_command('CopyGithubLink', function(args)
+		M.git_url_to_clipboard("main", nil, args.line1, args.line2)
+	end, { range = true })
 
-	vim.api.nvim_create_user_command('CopyGithubLinkWithBranchName', function()
-		M.git_url_to_clipboard()
-	end, {})
+	vim.api.nvim_create_user_command('CopyGithubLinkWithBranchName', function(args)
+		M.git_url_to_clipboard(nil, nil, args.line1, args.line2)
+	end, { range = true })
 end
 
 return M
